475422d7806356504d5f20e58c3b950844673e3a
MickoLaput <122064428+MickoLaput@users.noreply.github.com>
2025-08-30 21:20:21 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/forum', forumRoutes);
 app.use('/api', usersRoutes);
 app.use('/api', treatmentsRoutes);
-app.use('/uploads', express.static('uploads'));  
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 app.use('/api', diagnosesRoutes);
 
 const PORT = process.env.PORT || 3000;
